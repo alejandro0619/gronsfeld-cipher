5057088c5ed9c453f64a7d09385f8445f9a52231
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-04-02 15:35:39 -0400

    Delete main.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { generateTable } from "./tableReader";
-import { mapToNumbers } from "./mapping";
-
-
-
-const inputString = "Hello word how are you doing today";
-const key = "1235";
-const result = mapToNumbers(inputString, key);
-console.log(result);
